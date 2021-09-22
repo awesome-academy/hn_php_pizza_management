a65b1bfcef3c2ffe479943edeb59820232e46540
cuong2209 <59088513+cuongnc-2209@users.noreply.github.com>
2021-09-22 09:21:44 +0700

    localization

---
+++ resources/lang/vi/client.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'header' => [
+        'home' => 'Trang chủ',
+        'contact' => 'Liên hệ',
+        'login' => 'Đăng nhập',
+        'go_to_cart' => 'Đi tới giỏ hàng',
+    ],
+];
